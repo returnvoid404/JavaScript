b1e9d8ab392fe7c9c39a846cb06785b702f56fe8
Sami Hussain <153908936+returnvoid404@users.noreply.github.com>
2025-12-22 08:24:10 +0000

    Learned DataTypes basics

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all JS code to newer version
+
+// alert(3 + 3)
+/* can't use this for now becuase nowadays we make a file with .js extension which runs on nodejs
+while earlier we wrote JS code inside html and it runs on browser where we could use this */
+
+/*Primitive DataTypes */
+
+// number -> 2 to power of 53
+// bigint
+// string -> ""
+// boolean -> T/F
+// null -> Standalone Value
+// undefined -> Value not defined yet
+// symbol -> unique
+
+/*Object DataTypes */
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
+
