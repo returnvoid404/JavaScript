8334d0c1ad03cb1d3d048980ea76682335063acc
Sami Hussain <153908936+returnvoid404@users.noreply.github.com>
2025-12-22 08:07:22 +0000

    Learned about -> Let, Const and Var

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,10 @@
+const accountId = 1;
+let accountEmail = "sami@gmail.com";
+var accountPass = "Admin";
+accountCity = "Karachi";
+let accountState;
+//Prefer not to use var because
+//of functional and scope block issue
+
+console.log(accountEmail);
+console.table([accountId,accountEmail,accountPass,accountCity,accountState])
