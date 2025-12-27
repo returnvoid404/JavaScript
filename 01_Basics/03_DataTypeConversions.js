1a9e215d17a56ebfaa6a5926a53f061a75768bd3
returnvoid404 <hussainsami46@gmail.com>
2025-12-27 12:15:20 +0500

    Add examples for string concatenation and arithmetic operations in data type conversions

--- 01_Basics/03_DataTypeConversions.js
+++ 01_Basics/03_DataTypeConversions.js
@@ -44,3 +44,25 @@ console.log(booleanIsLoggedIn); //Gives False
 isLoggedIn = "Sami";
 booleanIsLoggedIn = Boolean(isLoggedIn);
 console.log(booleanIsLoggedIn); //Gives True
+
+console.log(2**2); //Exponentiation Operator
+
+
+console.log( 1 + "2" ); //String Concatenation
+console.log( "1" + 2 ); //String Concatenation
+console.log( 1 + 2 + "3" ); //String Concatenation
+console.log( "1" + 2 + 3 ); //String Concatenation
+
+console.log( Number("1") + 2 ); //Number Addition
+console.log( 1 + 2 + "2"); // Output 32
+
+// Whenever there is a string appearing first in addition, JS converts everything to string and performs concatenation.
+// But in other arithmetic operations like -,*,/ it converts everything to number and performs the operation.
+
+console.log( "6" / "2" ); //Gives 3
+console.log( "6" * "2" ); //Gives 12
+console.log( "6" - "2" ); //Gives 4
+console.log( "6" - 2 );   //Gives 4
+
+console.log( "6" + "2" ); //Gives 62 (String Concatenation)
+
